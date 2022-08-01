2a6120a0ca2efa079f962d42e33924050f8ff8c1
Mahammed Taheer <mohd.taheer@gmail.com>
2022-08-01 13:37:19 +0530

    Added revoke DB script.

--- db_release_scripts/mosip_keymgr/sql/1.2.0_keymgr-scripts_release.sql
+++ db_release_scripts/mosip_keymgr/sql/1.2.0_keymgr-scripts_release.sql
@@ -1,7 +1,6 @@
 \c mosip_keymgr sysadmin
 
 
-ALTER TABLE keymgr.key_alias ADD COLUMN cert_thumbprint character varying(100);
 ALTER TABLE keymgr.ca_cert_store ADD CONSTRAINT cert_thumbprint_unique UNIQUE (cert_thumbprint,partner_domain);
 
 ALTER TABLE keymgr.key_alias ADD COLUMN uni_ident character varying(50);
