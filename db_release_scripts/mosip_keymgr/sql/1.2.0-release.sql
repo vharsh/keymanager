ea0523c50158250c9b3e7b8cd4393cbbd2e68ab9
Mahammed Taheer <mohd.taheer@gmail.com>
2022-08-17 13:52:00 +0530

    Updated the release db scripts as per new requirements.

--- db_release_scripts/mosip_keymgr/sql/1.2.0-release.sql
+++ db_release_scripts/mosip_keymgr/sql/1.2.0-release.sql
@@ -1,6 +1,7 @@
 \c mosip_keymgr sysadmin
 
 
+ALTER TABLE keymgr.key_alias ADD COLUMN cert_thumbprint character varying(100);
 ALTER TABLE keymgr.ca_cert_store ADD CONSTRAINT cert_thumbprint_unique UNIQUE (cert_thumbprint,partner_domain);
 
 ALTER TABLE keymgr.key_alias ADD COLUMN uni_ident character varying(50);
@@ -9,17 +10,11 @@ ALTER TABLE keymgr.key_alias ADD CONSTRAINT uni_ident_const UNIQUE (uni_ident);
 ALTER TABLE keymgr.key_policy_def ADD COLUMN pre_expire_days smallint;
 ALTER TABLE keymgr.key_policy_def ADD COLUMN access_allowed character varying(1024);
 
-insert into keymgr.key_policy_def(app_id, key_validity_duration, is_active, cr_by, cr_dtimes, pre_expire_days, access_allowed) values('ADMIN_SERVICES',1095,TRUE,'mosipadmin',now(),60,'NA');
-insert into keymgr.key_policy_def(app_id, key_validity_duration, is_active, cr_by, cr_dtimes, pre_expire_days, access_allowed) values('RESIDENT',1095,TRUE,'mosipadmin',now(),60,'NA');
-insert into keymgr.key_policy_def(app_id, key_validity_duration, is_active, cr_by, cr_dtimes, pre_expire_days, access_allowed) values('COMPLIANCE_TOOLKIT',1095,TRUE,'mosipadmin',now(),60,'NA');
+INSERT INTO keymgr.key_policy_def
+(app_id, key_validity_duration, is_active, pre_expire_days, access_allowed, cr_by, cr_dtimes, upd_by, upd_dtimes, is_deleted, del_dtimes)
+VALUES('DIGITAL_CARD', 1095, true, 60, 'NA', 'mosipadmin', '2022-07-12 05:40:58.155', NULL, NULL, false, NULL);
 
+INSERT INTO keymgr.key_policy_def_h
+(app_id, eff_dtimes, key_validity_duration, is_active, pre_expire_days, access_allowed, cr_by, cr_dtimes, upd_by, upd_dtimes, is_deleted, del_dtimes)
+VALUES('DIGITAL_CARD', '2022-07-12 05:40:58.195', 1095, true, NULL, NULL, 'mosipadmin', '2022-07-12 05:40:58.195', NULL, NULL, false, NULL);
 
--- updating default values for pre_expire_days & access_allowed columns
-update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='PRE_REGISTRATION';
-update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='REGISTRATION';
-update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='REGISTRATION_PROCESSOR';
-update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='ID_REPO';
-update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='KERNEL';
-update keymgr.key_policy_def set pre_expire_days=1125, access_allowed='NA' where app_id='ROOT';
-update keymgr.key_policy_def set pre_expire_days=30, access_allowed='NA' where app_id='BASE';
-update keymgr.key_policy_def set pre_expire_days=395, access_allowed='NA' where app_id='PMS';
