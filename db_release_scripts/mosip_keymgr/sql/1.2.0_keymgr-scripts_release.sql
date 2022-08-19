c8e02cb3d94bb17e7ab0fd269a926f2ab8d8a16f
dhanendra06 <dhanendra.tech@gmail.com>
2022-08-19 09:54:14 +0530

    Added digitalcard policy

--- db_release_scripts/mosip_keymgr/sql/1.2.0_keymgr-scripts_release.sql
+++ db_release_scripts/mosip_keymgr/sql/1.2.0_keymgr-scripts_release.sql
@@ -9,3 +9,12 @@ ALTER TABLE keymgr.key_alias ADD CONSTRAINT uni_ident_const UNIQUE (uni_ident);
 
 ALTER TABLE keymgr.key_policy_def ADD COLUMN pre_expire_days smallint;
 ALTER TABLE keymgr.key_policy_def ADD COLUMN access_allowed character varying(1024);
+
+INSERT INTO keymgr.key_policy_def
+(app_id, key_validity_duration, is_active, pre_expire_days, access_allowed, cr_by, cr_dtimes, upd_by, upd_dtimes, is_deleted, del_dtimes)
+VALUES('DIGITAL_CARD', 1095, true, 60, 'NA', 'mosipadmin', '2022-07-12 05:40:58.155', NULL, NULL, false, NULL);
+
+INSERT INTO keymgr.key_policy_def_h
+(app_id, eff_dtimes, key_validity_duration, is_active, pre_expire_days, access_allowed, cr_by, cr_dtimes, upd_by, upd_dtimes, is_deleted, del_dtimes)
+VALUES('DIGITAL_CARD', '2022-07-12 05:40:58.195', 1095, true, NULL, NULL, 'mosipadmin', '2022-07-12 05:40:58.195', NULL, NULL, false, NULL);
+
