b342234bc12faeae7a8103dc69284a7496854b6a
Mahammed Taheer <mohd.taheer@gmail.com>
2023-05-23 17:54:56 +0530

    Added conditions for zk data keystore cache.

--- kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/repository/DataEncryptKeystoreRepository.java
+++ kernel/kernel-keymanager-service/src/main/java/io/mosip/kernel/keymanagerservice/repository/DataEncryptKeystoreRepository.java
@@ -24,7 +24,7 @@ public interface DataEncryptKeystoreRepository extends JpaRepository<DataEncrypt
 	 * @param id the id
 	 * @return the string
 	 */
-	@Cacheable(value="zkdataencryptedkeystorecache", key="#id")
+	@Cacheable(value="zkdataencryptedkeystorecache", key="#id", condition="#id!=null")
 	@Query("SELECT d.key from DataEncryptKeystore d where d.id = :id")
 	String findKeyById(@Param("id") Integer id);
 
