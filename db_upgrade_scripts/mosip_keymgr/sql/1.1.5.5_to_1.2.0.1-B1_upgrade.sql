2beb4e471b00576eb6167b10464e63ca68d74ac7
mahammedtaheer <57249563+mahammedtaheer@users.noreply.github.com>
2023-07-20 16:04:40 +0530

    Jira No. MOSIP-28101. Added Digital Card App value and deleted credential service app id. (#195)

--- db_upgrade_scripts/mosip_keymgr/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
+++ db_upgrade_scripts/mosip_keymgr/sql/1.1.5.5_to_1.2.0.1-B1_upgrade.sql
@@ -38,6 +38,8 @@ update keymgr.key_policy_def set pre_expire_days=395, access_allowed='NA' where 
 update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='ADMIN_SERVICES';
 update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='RESIDENT';
 update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='COMPLIANCE_TOOLKIT';
+update keymgr.key_policy_def set pre_expire_days=60, access_allowed='NA' where app_id='DIGITAL_CARD';
 
 delete from keymgr.key_policy_def where app_id='IDA';
 delete from keymgr.key_policy_def where app_id='DATASHARE';
+delete from keymgr.key_policy_def where app_id='CREDENTIAL_SERVICE';
